5a7d965bde81321c0388dec6409182c9620498c2
Nikhil Chaudhary <148001022+nickgithubio@users.noreply.github.com>
2024-04-10 15:36:04 +0530

    Day-7 (10/04/24)

---
+++ App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Languages from './component/Languages';
+import Message from './component/Message'
+
+function App() {
+  
+
+  return (
+<>  
+<Message/>
+<Languages/>
+    </>
+  );
+}
+
+export default App
---
+++ component/Languages.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Languages = () => {
+    let lang=["java","javascript", "kotlin", "python", "c"];
+  return (
+    <div>
+        <h1>Best Programming Languages:</h1>
+        <ul>
+            {lang.map((item,index)=>(
+            <li key={index}>{item}</li>
+            ))}
+        </ul>
+
+    </div>
+  )
+}
+
+export default Languages
